2f22f18577b9c87bc89d8da0b15481b1c39e1c4a
otills <oliver.tills@plymouth.ac.uk>
2018-09-28 11:30:43 +0100

    Update setup.py

--- program/setup.py
+++ program/setup.py
@@ -1,4 +1,4 @@
-conda create -n -y opencvtest numpy scipy scikit-learn matplotlib python=2.7
+conda create -n opencvtest -y numpy scipy scikit-learn matplotlib python=2.7
 source activate opencvtest
 conda install -c -y menpo opencv3 
 conda install -c groakat -y pathos=0.2a1
