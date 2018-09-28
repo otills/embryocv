8ef879c0cb1656003c5156e6fb462aa655f6038c
otills <oliver.tills@plymouth.ac.uk>
2018-09-28 14:05:11 +0100

    Add files via upload

--- program/setup.py
+++ program/setup.py
@@ -1,5 +1,7 @@
-conda create -n opencvtest -y numpy scipy scikit-learn matplotlib python=2.7
-source activate opencvtest
-conda install -c -y menpo opencv3 
+conda create -n opencvtest2 -y numpy scipy scikit-learn matplotlib python=2.7
+
+source activate opencvtest2
+
+conda install -c menpo -y opencv3 
 conda install -c groakat -y pathos=0.2a1
 conda install -y pyqtgraph qtpy qt
